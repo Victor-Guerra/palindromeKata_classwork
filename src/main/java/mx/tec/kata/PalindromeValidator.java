90c8babaa2a170d2d5feac0e8cd13d6a3a93189f
Victor-Guerra <v.e.g.a.2000@hotmail.com>
2021-04-16 17:38:39 -0600

    Step 5 fixed

--- src/main/java/mx/tec/kata/PalindromeValidator.java
+++ src/main/java/mx/tec/kata/PalindromeValidator.java
@@ -3,8 +3,8 @@ package mx.tec.kata;
 public class PalindromeValidator {
     public boolean validate(String string) {
         String clearString = string.replaceAll("\\s+","");
-        for ( int i = 0; i < string.length() / 2; i++) {
-            if(string.charAt(i) != string.charAt(string.length() - 1 - i)) {
+        for ( int i = 0; i < clearString.length() / 2; i++) {
+            if(clearString.charAt(i) != clearString.charAt(clearString.length() - 1 - i)) {
                 return false;
             }
         }
