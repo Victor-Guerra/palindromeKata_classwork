f25fb48adf4b8f2984a30bc356d743fc88377cc4
Victor-Guerra <v.e.g.a.2000@hotmail.com>
2021-04-16 16:39:05 -0600

    Initial Commit

---
+++ src/main/java/mx/tec/kata/PalindormeKataApplication.java
@@ -0,0 +1,13 @@
+package mx.tec.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalindormeKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalindormeKataApplication.class, args);
+	}
+
+}
